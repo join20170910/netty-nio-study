3f417544f4c608d836d40164a7aacd27e7b9d920
apple <17717990@qq.com>
2023-02-10 23:00:55 +0800

    Redisson功能测试

---
+++ README.md
@@ -0,0 +1,9 @@
+brew install redis
+Starting and stopping Redis using launchd
+As an alternative to running Redis in the foreground, you can also use launchd to start the process in the background:
+
+brew services start redis
+This launches Redis and restarts it at login. You can check the status of a launchd managed Redis by running the following:
+
+brew services info redis
+If the service is running, you'll see output like the following:
--- im-system-new/im-tcp-new/src/test/java/com/ws/st/tcp/RedissonTest.java
+++ im-system-new/im-tcp-new/src/test/java/com/ws/st/tcp/RedissonTest.java
@@ -5,9 +5,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.redisson.Redisson;
 import org.redisson.api.RBucket;
+import org.redisson.api.RMap;
+import org.redisson.api.RTopic;
 import org.redisson.api.RedissonClient;
+import org.redisson.api.listener.MessageListener;
 import org.redisson.client.codec.StringCodec;
 import org.redisson.config.Config;
+
+import java.time.Instant;
 
 public class RedissonTest {
 
@@ -33,6 +38,29 @@ public class RedissonTest {
     @Test
     @DisplayName("Redisson map 测试")
     public void demo02(){
-        redissonClient.getMap()
+        RMap<String, Object> imMap = redissonClient.getMap("imMap");
+        System.out.println(imMap.get("name"));
+        imMap.put("name","john");
+        System.out.println(imMap.get("name"));
+        imMap.expire(Instant.now());
+    }
+    @Test
+    @DisplayName("Redisson 发布订阅 测试")
+    public void demo03(){
+        RTopic topic = redissonClient.getTopic("topic");
+        topic.addListener(String.class, new MessageListener<String>() {
+            @Override
+            public void onMessage(CharSequence channel, String msg) {
+                System.out.println("1 收到 消息 ：" + msg);
+            }
+        });
+        RTopic topic2 = redissonClient.getTopic("topic");
+        topic2.addListener(String.class, new MessageListener<String>() {
+            @Override
+            public void onMessage(CharSequence channel, String msg) {
+                System.out.println("2 收到 消息 ：" + msg);
+            }
+        });
+        topic.publish("hello world");
     }
 }
