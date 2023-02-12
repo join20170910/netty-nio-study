cb7b0e06ded8ef060944e3a14ed4071bc55ca024
apple <17717990@qq.com>
2023-02-12 20:26:53 +0800

    session 和 用户对应的关系容器调整及 删除方法增加

--- im-system-new/im-tcp-new/src/main/java/com/ws/st/tcp/utils/SessionSocketHolder.java
+++ im-system-new/im-tcp-new/src/main/java/com/ws/st/tcp/utils/SessionSocketHolder.java
@@ -1,5 +1,6 @@
 package com.ws.st.tcp.utils;
 
+import com.ws.st.common.model.UserClientDto;
 import io.netty.channel.socket.nio.NioSocketChannel;
 
 import java.util.Map;
@@ -9,13 +10,45 @@ import java.util.concurrent.ConcurrentHashMap;
  * NioSocketChannel 容器
  */
 public class SessionSocketHolder {
-    private static final Map<String, NioSocketChannel> CHANNELS = new ConcurrentHashMap<>();
+    private static final Map<UserClientDto, NioSocketChannel> CHANNELS = new ConcurrentHashMap<>();
 
-    public static void put(String userId, NioSocketChannel channel){
-        CHANNELS.put(userId,channel);
+    public static void put(String userId,Integer clientType,Integer appId, NioSocketChannel channel){
+        UserClientDto userClientDto = new UserClientDto();
+        userClientDto.setUserId(userId);
+        userClientDto.setAppId(appId);
+        userClientDto.setClientType(clientType);
+        CHANNELS.put(userClientDto,channel);
     }
 
-    public static NioSocketChannel get(String userId){
-        return CHANNELS.get(userId);
+    public static NioSocketChannel get(String userId,Integer clientType,Integer appId){
+        UserClientDto userClientDto = new UserClientDto();
+        userClientDto.setUserId(userId);
+        userClientDto.setAppId(appId);
+        userClientDto.setClientType(clientType);
+        return CHANNELS.get(userClientDto);
+    }
+
+    /**
+     * 删除
+     * @param userId
+     * @param clientType
+     * @param appId
+     */
+    public static void remove(String userId,Integer clientType,Integer appId){
+        UserClientDto userClientDto = new UserClientDto();
+        userClientDto.setUserId(userId);
+        userClientDto.setAppId(appId);
+        userClientDto.setClientType(clientType);
+        CHANNELS.remove(userClientDto);
+    }
+
+    /**
+     * 删除 channel
+     * @param channel
+     */
+    public static void remove(NioSocketChannel channel){
+        CHANNELS.entrySet().stream().
+                filter(entity -> entity.getValue() == channel)
+                .forEach(entry ->CHANNELS.remove(entry.getKey()));
     }
 }
